235359da0510705044984a5db07f56e352447715
dmj0shua1 <dmj0shua1@gmail.com>
2018-03-22 09:12:24 +0800

    Before Forgot password

---
+++ app/src/main/java/com/loopbookinc/sonder/app/AppConfig.java
@@ -0,0 +1,18 @@
+package com.loopbookinc.sonder.app;
+
+/**
+ * Created by Loopbook on 3/8/2018.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://ec2-34-216-225-113.us-west-2.compute.amazonaws.com/veeds/data_proc.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://ec2-34-216-225-113.us-west-2.compute.amazonaws.com/veeds/data_proc.php";
+
+    // Server forgot password url
+    public static String URL_FORGOT_PW = "http://ec2-34-216-225-113.us-west-2.compute.amazonaws.com/veeds/request_reset_code.php";
+
+
+}
