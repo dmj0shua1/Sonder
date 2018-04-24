af0d00284b2125a438ddaf9662164d94bce27032
dmj0shua1 <dmj0shua1@gmail.com>
2018-04-24 16:46:35 +0800

    - Search bar, but a dummy

--- app/src/main/java/com/loopbookinc/sonder/HomeFragment.java
+++ app/src/main/java/com/loopbookinc/sonder/HomeFragment.java
@@ -14,6 +14,7 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * Created by Loopbook on 4/24/2018.
@@ -24,11 +25,18 @@ public class HomeFragment extends Fragment implements GoogleMap.OnMyLocationButt
         OnMapReadyCallback {
     private GoogleMap mMap;
 
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
     }
+
+
 
     @Override
     public boolean onMyLocationButtonClick() {
